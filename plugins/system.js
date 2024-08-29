b061d45e886f7cfea0cad0716f4a6295a0a69dd4
QUEEN-KYLIE-MD <queenkyliemd01@gmail.com>
2024-08-29 12:19:53 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -16,10 +16,10 @@ let status =`┌────────────────────
     *├ ⏳Uptime:-  ${runtime(process.uptime())}*    
     *├ 📟Ram usage:- ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
     *├ ⚙️HostName:- ${os.hostname()}*
-    *├ 👨‍💻Owner :- Sahas Tech*   
+    *├ 👨‍💻Owner :- *Sahas Nethsara (Sahas Tech)*   
     *├ 🤖version: 1.5.0*
     └───────────────────────
-    *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜ*
+    *©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*
 `
 return reply(`${status}`)
 }catch(e){
