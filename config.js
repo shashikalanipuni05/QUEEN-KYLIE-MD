ad1c43ac98e4d8b6b6542b177f61453c51b4ec5b
SagaraSubash <110216100+SagaraSubash@users.noreply.github.com>
2024-08-30 15:18:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "ieJglBQK#AzpAK5FhWMsU7_YTLkT-H3Hy4vRyEeos1ZYMReJfu84",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/9b44e3266c065df601430.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94718913389\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
 SUDO_NB: process.env.SUDO_NB || "94718913389",
