f948e0476188fa6d5d117aafad36a504969036e3
QUEEN-KYLIE-MD <queenkyliemd01@gmail.com>
2024-09-01 09:04:24 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -6,7 +6,7 @@ cmd({
     pattern: "song",
     desc: "download songs",
     category: "download",
-    react: "🎼",
+    react: "🎶",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
