084ccb0b07ae2b2fd6a181c5a48123959e2d836f
QUEEN-KYLIE-MD <queenkyliemd01@gmail.com>
2024-09-01 18:51:05 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -4,26 +4,55 @@ const {runtime} = require('../lib/functions')
 
 cmd({
     pattern: "system",
-    alias: ["status","botinfo"],
-    desc: "check up time",
-    category: "main",
-    react: "👀",
+    react: "🧬",
+    alias: ["status"],
+    desc: "Check bot system status.",
+    category: "genaral",
+    use: '.system',
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{ 
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply,react}) => {
+try{
+    if(isGroup){
+        const fsh = await fetchJson(`${config.DOWNLOADSAPI}${bot}/${from}`); 
+        if(fsh &&  (fsh?.error || fsh?.data?.type == 'false')) return;
+         
+ }else if(!isGroup){
+        const fshh = await fetchJson(`${config.DOWNLOADSAPI}${bot}/${sender}`); 
+        if(fshh &&  (fshh?.error || fshh?.data?.type == 'false')) return;
+      }
+    var start = new Date().getTime();
+var end = new Date ().getTime();
+const ping = (end - start)
 
-let status =`┌────────────────────
-    *├ ⏳Uptime:-  ${runtime(process.uptime())}*    
-    *├ 📟Ram usage:- ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
-    *├ ⚙️HostName:- ${os.hostname()}*
-    *├ 👨‍💻Owner:- Sahas Nethsara*   
-    *├ 🤖Version:- 1.0.0*
-    └───────────────────────
-    *©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`
-return reply(`${status}`)
-}catch(e){
-console.log(e)
-reply(`${e}`)
+if (os.hostname().length == 12) {
+  hostname = mg.replithostname
+} else {
+  if (os.hostname().length == 36) {
+    hostname = mg.herokuhostname
+} else {
+    if (os.hostname().length == 8) {
+      hostname = mg.koyebhostname
+} else {
+      hostname = os.hostname()
+}}}
+
+const ram = `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB`
+const rtime = await runtime(process.uptime())
+
+const txt = `╭───────────────────●●►
+*├⚙️𝙿𝙻𝙰𝚃𝙵𝙾𝚁𝙼 : ${hostname}*  
+*├⏰𝚄𝙿𝚃𝙸𝙼𝙴 : ${rtime}*       
+*├📟𝚁𝙰𝙼 : ${ram}*             
+*├🌐𝙼𝙾𝙳𝙴 : ${mg.mode}* 
+*├ 👾𝚅𝙴𝚁𝚂𝙸𝙾𝙽 : 1.0.0*              
+*├👨‍💻𝙾𝚆𝙽𝙴𝚁𝚂 : masterx team*
+╰───────────────────●●►
+
+*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`
+await conn.sendMessage(m.chat, { text:txt }, { quoted: mek })
+} catch (e) {
+reply('*Error !!*')
+l(e)
 }
 })
