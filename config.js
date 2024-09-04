2b130c51463e66e0d5cb5d09cbda1deac8129f95
Sahas_Tech <queenkyliemd01@gmail.com>
2024-09-04 18:56:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session id",
+SESSION_ID: process.env.SESSION_ID || "2IFnBaaZ#NSQpCFT5Px8UNIeihoWzV5_PSsnUVW3ni6bD0lMmYAE",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/198a322e8cc0f2bd6cba2.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94765527900",
 SUDO_NB: process.env.SUDO_NB || "94718913389",
