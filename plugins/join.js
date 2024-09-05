a0e9bce2d19efbcbf5d1228697fe86e3cfabb8d5
Sahas_Tech <queenkyliemd01@gmail.com>
2024-09-05 20:01:49 +0530

    Create join.js

---
+++ plugins/join.js
@@ -0,0 +1,19 @@
+cmd({
+    pattern: "join",
+    desc: "joins group by link",
+    category: "owner",
+    react:"🔗",
+    use: '<group link.>',
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+if (!isOwner) return;    
+try{  
+if (!q){ 
+if(m.quoted && m.quoted.msg) {
+q = m.quoted.msg
+}else{
+const mass = await conn.sendMessage(m.chat, { text: mg.wagrouplinknotfound }, { quoted: mek });
+return await conn.sendMessage(m.chat, { react: { text: "⁉️", key: mass.key } });
+
+}
+}
